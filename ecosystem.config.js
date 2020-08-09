2d353f5c44b80a531d78dc9af548611e82ad1020
Entom <office@entom.dev>
2020-08-09 19:55:17 +0200

    feat(app): add extra configuration for other app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: "app",
+        name: "app01",
         script: "./app.js",
         env: {
             NODE_ENV: "development",
@@ -9,6 +9,17 @@ module.exports = {
         env_production: {
             NODE_ENV: "production",
             PORT: 8081
+        }
+    }, {
+        name: "app02",
+        script: "./app.js",
+        env: {
+            NODE_ENV: "development",
+            PORT: 8082
+        },
+        env_production: {
+            NODE_ENV: "production",
+            PORT: 8082
         }
     }]
 }
